681b7af2e30a80c16dfe53eae87b55a1b424bc85
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-09-30 16:13:25 +0100

    error handling

---
+++ ErrorHandling.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double number = 38498.3222;
+
+      string dinosaur = "Barney";
+
+      double digits = 293.000;
+
+      bool isYes = true;
+
+      string band = "The Low Anthem";
+
+      Console.WriteLine($"number: {number} dinosaur:{dinosaur}, digits:{digits}, isYes:{isYes}, band:{band}");
+
+    }
+  }
+}
