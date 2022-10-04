6f329c5db155bd819b2a35377eb6b96610e45de6
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-04 12:53:35 +0100

    built in methods

--- ChangingDataTypes.cs
+++ ChangingDataTypes.cs
@@ -1,23 +1,23 @@
-using System;
+// using System;
 
-namespace FavoriteNumber
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Ask user for fave number
-      Console.Write("Enter your favorite number!: ");
+// namespace FavoriteNumber
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Ask user for fave number
+//       Console.Write("Enter your favorite number!: ");
      
 
 
-      // Turn that answer into an int
-      //ReadLine is always a string so using inbuilt function convert allows us to get around this
-      int faveNumber = Convert.ToInt32(Console.ReadLine());
+//       // Turn that answer into an int
+//       //ReadLine is always a string so using inbuilt function convert allows us to get around this
+//       int faveNumber = Convert.ToInt32(Console.ReadLine());
 
-      Console.Write($"I like {faveNumber} too!");
+//       Console.Write($"I like {faveNumber} too!");
 
 
-    }
-  }
-}
+//     }
+//   }
+// }
--- Modulo.cs
+++ Modulo.cs
@@ -1,20 +1,20 @@
-using System;
+// using System;
 
-namespace ClassTeams
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Number of students
-int students = 18;
+// namespace ClassTeams
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Number of students
+// int students = 18;
 
-      // Number of students in a group
-int groupSize = 3;
+//       // Number of students in a group
+// int groupSize = 3;
 
-      // Does groupSize go evenly into students?
-Console.WriteLine(students % groupSize);
+//       // Does groupSize go evenly into students?
+// Console.WriteLine(students % groupSize);
 
-    }
-  }
-}
+//     }
+//   }
+// }
