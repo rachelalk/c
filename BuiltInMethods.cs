54ca01521af0fcb993ecef269dd833ec972123dc
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-04 12:58:53 +0100

    more built in methods

--- BuiltInMethods.cs
+++ BuiltInMethods.cs
@@ -1,23 +1,52 @@
+// using System;
+
+// namespace LowestNumber
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Starting variables 
+//       int numberOne = 12932;
+//       int numberTwo = -2828472;
+
+//       // Use built-in methods and save to variable 
+// double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+
+//       // Use built-in methods and save to variable 
+// double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+//       // Print the lowest number
+// Console.WriteLine(Math.Min(numberOneSqrt, numberTwo));
+
+//     }
+//   }
+// }
+
+
+
+//More built in methods
+
 using System;
 
-namespace LowestNumber
+namespace DocumentationHunt
 {
   class Program
   {
     static void Main(string[] args)
     {
-      // Starting variables 
-      int numberOne = 12932;
-      int numberTwo = -2828472;
 
-      // Use built-in methods and save to variable 
-double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+      double numberOne = 6.5;
+      double numberTwo = 4;
 
-      // Use built-in methods and save to variable 
-double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+      // Raise numberOne to the numberTwo power
+      Console.WriteLine(Math.Pow(numberOne, numberTwo));
 
-      // Print the lowest number
-Console.WriteLine(Math.Min(numberOneSqrt, numberTwo));
+      // Round numberOne up
+      Console.WriteLine(Math.Ceiling(numberOne));
+
+      // Find the larger number between numberOne and numberTwo
+      Console.WriteLine(Math.Max(numberOne, numberTwo));
 
     }
   }
