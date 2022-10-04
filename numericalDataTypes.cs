1b3b98eb2868fab048772d6716d61d90afa35a77
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-04 12:26:45 +0100

    arithmetic operators

--- numericalDataTypes.cs
+++ numericalDataTypes.cs
@@ -1,26 +1,26 @@
-using System;
+// using System;
 
-namespace Numbers
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Number of pizza shops
-int pizzaShops = 4332;
+// namespace Numbers
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Number of pizza shops
+// int pizzaShops = 4332;
 
-      // Number of employees
-int totalEmployees = 86928;
+//       // Number of employees
+// int totalEmployees = 86928;
 
-      // Revenue
-decimal revenue = 390819.28m;
+//       // Revenue
+// decimal revenue = 390819.28m;
 
-      // Log the values to the console:
-Console.WriteLine(pizzaShops);
+//       // Log the values to the console:
+// Console.WriteLine(pizzaShops);
 
-Console.WriteLine(totalEmployees);
+// Console.WriteLine(totalEmployees);
 
-Console.WriteLine(revenue);
-   	}
-  }
-}
+// Console.WriteLine(revenue);
+//    	}
+//   }
+// }
