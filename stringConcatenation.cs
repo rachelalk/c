aff8e1366f498ab552791f173020d14a514096b5
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-07 14:05:46 +0100

    string concatenation

---
+++ stringConcatenation.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare the variables
+string beginning = "Once there was a boy called Lenny.";
+
+string middle = "His Mum was a feral and he needed a new home.";
+
+string end = "Luckily, he was adopted by a lovely couple and had a brilliant life.";
+
+      // Concatenate the string and the variables
+
+string story = beginning + " " + middle + " " + end;
+
+      // Print the story to the console 
+Console.WriteLine(story);
+    }
+  }
+}
