555e17396dbcd8782ca2d637b15edb4c129c348a
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-07 14:54:28 +0100

    parts of strings

--- IndexOf.cs
+++ IndexOf.cs
@@ -1,23 +1,23 @@
-using System;
+// using System;
 
-namespace PasswordCheck
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Create password
-      string password = "a92301j2add";
+// namespace PasswordCheck
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Create password
+//       string password = "a92301j2add";
 
-      // Get password length
-int passwordLength = password.Length;
+//       // Get password length
+// int passwordLength = password.Length;
 
-      // Check if password uses symbol
-int passwordCheck = password.IndexOf("!");
+//       // Check if password uses symbol
+// int passwordCheck = password.IndexOf("!");
 
-      // Print results
-      Console.WriteLine($"The user password is {password}. Its length is {passwordLength} and it receives a {passwordCheck} check.");
+//       // Print results
+//       Console.WriteLine($"The user password is {password}. Its length is {passwordLength} and it receives a {passwordCheck} check.");
 
-    }
-  }
-}
+//     }
+//   }
+// }
