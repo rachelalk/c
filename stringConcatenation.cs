326b671ceb8793900e600d55c2776b093c73f41b
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-10-07 14:11:04 +0100

    string interpolation

--- stringConcatenation.cs
+++ stringConcatenation.cs
@@ -1,24 +1,24 @@
-using System;
+// using System;
 
-namespace StoryTime
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      // Declare the variables
-string beginning = "Once there was a boy called Lenny.";
+// namespace StoryTime
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Declare the variables
+// string beginning = "Once there was a boy called Lenny.";
 
-string middle = "His Mum was a feral and he needed a new home.";
+// string middle = "His Mum was a feral and he needed a new home.";
 
-string end = "Luckily, he was adopted by a lovely couple and had a brilliant life.";
+// string end = "Luckily, he was adopted by a lovely couple and had a brilliant life.";
 
-      // Concatenate the string and the variables
+//       // Concatenate the string and the variables
 
-string story = beginning + " " + middle + " " + end;
+// string story = beginning + " " + middle + " " + end;
 
-      // Print the story to the console 
-Console.WriteLine(story);
-    }
-  }
-}
+//       // Print the story to the console 
+// Console.WriteLine(story);
+//     }
+//   }
+// }
